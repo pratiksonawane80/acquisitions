5c282c811f881fc336881eaf51448eae242f6678
pratiksonawane80 <pratiksonawane80@gmail.com>
2025-09-27 20:50:38 +0530

    prettier eslint implement

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello! Server is running");
+app.get('/', (req, res) => {
+  res.status(200).send('Hello! Server is running');
 });
 
 export default app;
